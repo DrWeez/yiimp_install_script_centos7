8542977e6b1cfc2194a83747ee7a7ca0b7495a51
xavatar94 <xavatar@wanadoo.fr>
2017-12-26 20:52:30 +0100

    add sql creation import

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ sudo sed -i 's/server = yaamp.com/server = 'vps496236.ovh.net'/g' *.conf
 sudo sed -i 's/host = yaampdb/host = localhost/g' *.conf
 sudo sed -i 's/database = yaamp/database = utopooldb/g' *.conf
 sudo sed -i 's/username = root/username = stratuser/g' *.conf
-sudo sed -i 's/password = patofpaq/password = Gihala84/g' *.conf
+sudo sed -i 's/password = patofpaq/password = '$password2'/g' *.conf
 sleep 3
 
 sudo touch /var/log/debug.log
@@ -58,6 +58,11 @@ sudo chmod -R 775 /var/stratum
 sudo chmod -R 777 /var/web/yaamp/runtime
 sudo chmod -R 775 /var/web/serverconfig.php
 sudo chmod -R 644 /var/log/debug.log
+sleep 3
+
+cd $HOME/install
+sudo wget https://github.com/xavatar/yiimp_install_scrypt_centos7/blob/master/serverconfig.php
+cp serverconfig.php /var/web/
 sudo chmod -R 775 /var/web/serverconfig.php
 sudo ln -s /var/web/ /var/www/html/
 sleep 3
@@ -76,8 +81,32 @@ Q1="GRANT ALL ON *.* TO 'stratum'@'localhost' IDENTIFIED BY '$password2';"
 Q2="FLUSH PRIVILEGES;"
 SQL="${Q1}${Q2}"
 sudo mysql -u root -p -e "$SQL"  
-
 sleep 3
+
+echo "Import Database "
+read -e -p "Enter Password MYSQL : " dbpass
+ echo "Peforming the SQL import"
+ echo ""
+ cd $HOME/install/yiimp/sql
+ gunzip 2016-04-03-yaamp.sql.gz
+     # import sql dump
+     sudo mysql -u root -p$dbpass utopooldb < 2016-04-03-yaamp.sql
+    # oh the humanity!
+     sudo mysql -u root -p$dbpass utopooldb < 2016-04-24-market_history.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2016-04-27-settings.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2016-05-11-coins.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2016-05-15-benchmarks.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2016-05-23-bookmarks.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2016-06-01-notifications.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2016-06-04-bench_chips.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2016-11-23-coins.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2017-02-05-benchmarks.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2017-03-31-earnings_index.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2017-05-accounts_case_swaptime.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2017-06-payouts_coinid_memo.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2017-09-notifications.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2017-10-bookmarks.sql
+     sudo mysql -u root -p$dbpass utopooldb < 2017-11-segwit.sql
 	
 echo '
 [clienthost1]
